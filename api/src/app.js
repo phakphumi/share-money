d493453b8b1422373d9c26665c185e479d7f44f4
pakpoom thaweesitthichat <pakpoom.thawee@gmail.com>
2022-04-15 20:20:33 +0700

    apply records routes to app and edit start script

--- api/src/app.js
+++ api/src/app.js
@@ -1,10 +1,9 @@
 import express from 'express';
+import recordsRouter from './routes/records.routes.js';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.json('hello world')
-})
+app.use('/records', recordsRouter)
 
 app.listen(8080, () => {
   console.log(`Server listening at port: ${8080}`)
